411ca0400daca5e0e7f4c89d25321eb2253ea9db
Kiana8181 <kianahashemi8@gmail.com>
2024-02-19 10:49:37 +0330

    first commit

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCard = () => {
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("Clicked")}>
+      Add to Card
+    </button>
+  );
+};
+
+export default AddToCard;
