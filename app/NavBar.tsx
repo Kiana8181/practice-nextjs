3e0d5bace10f2e5af0f07574c6776ecc49686097
Kiana8181 <kianahashemi8@gmail.com>
2024-06-05 03:35:00 +0330

    add upload file with cloudinary

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 gap-2">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
+      <Link href="/admin">Admin</Link>
       <Link href="/users">Users</Link>
+      <Link href="/upload">Upload</Link>
     </div>
   );
 };
---
+++ app/upload/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  console.log(publicId);
+  return (
+    <>
+      {publicId && (
+        <CldImage alt={"picture"} src={publicId} height={270} width={180} />
+      )}
+      <CldUploadWidget
+        uploadPreset="znujd07p"
+        onUpload={(result) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+
+          setPublicId(info.public_id);
+        }}
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
