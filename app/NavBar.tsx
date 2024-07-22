a51bf869816b243eee55cab4df6ac9270904aa70
Kiana8181 <kianahashemi8@gmail.com>
2024-07-22 16:20:01 +0330

    finish Authentication with Next Auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,32 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
-    <div className="flex bg-slate-200 p-5 gap-2">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/admin">Admin</Link>
       <Link href="/users">Users</Link>
       <Link href="/upload">Upload</Link>
+      {status === "loading" && <div>...Loading</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Sign Out
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
